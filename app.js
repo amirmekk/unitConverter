40008af1bd90e0b87b5a449daec0a1cd41c0e2b1
amir mekk <ozdzamir@gmail.com>
2016-07-23 12:05:01 +0100

    working v1 : can convert two first values , the button still dosnt work

--- app.js
+++ app.js
@@ -5,4 +5,34 @@
  */
  
 
- var firstInput = $(document).$('#firstInput') ;
+var firstInput = document.getElementById('firstInput') , secondInput = document.getElementById("secondInput") ,
+ firstVar , secondVar , weightButton = document.getElementById('weightButton') ; 
+
+
+firstInput.oninput = fromKtoM ;
+secondInput.oninput = fromMtoK ;
+ 
+function fromKtoM(){
+	firstVar = 0.62137119 , secondVar = 1.609344 ;
+	secondInput.value =  (Math.round(parseFloat(firstInput.value) * firstVar * 100) /100) || 0   ;
+};
+function fromMtoK(){
+	firstVar = 0.62137119 , secondVar = 1.609344 ;
+	firstInput.value =  (Math.round(parseFloat(secondInput.value) * secondVar * 100) /100) || 0  ;
+};
+
+function change(){
+	if(firstInput.placeholder === 'Enter kilometers here') {
+	firstInput.placeholder = 'Enter kilograms here' ;
+	secondInput.placeholder = 'Enter pounds here' ;
+	weightButton.innerHTML = "kilometers/Miles" ;
+		
+	} else if(firstInput.placeholder ===  'Enter kilograms here') {
+	firstInput.placeholder = 'Enter kilometers here' ;
+	secondInput.placeholder = 'Enter miles here' ;	
+	weightButton.innerHTML = "kilograms/pounds"
+	}
+	
+} ;
+$('#weightButton').on('click' , change)  ;
+
