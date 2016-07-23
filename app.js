943e1e0981508d5a0814c853b67e43e32be50221
amir mekk <ozdzamir@gmail.com>
2016-07-23 12:56:44 +0100

    the button of values change is now working and added clear input values button

--- app.js
+++ app.js
@@ -7,32 +7,48 @@
 
 var firstInput = document.getElementById('firstInput') , secondInput = document.getElementById("secondInput") ,
  firstVar , secondVar , weightButton = document.getElementById('weightButton') ; 
-
-
-firstInput.oninput = fromKtoM ;
-secondInput.oninput = fromMtoK ;
+	
+	firstInput.oninput = fromKtoM ;
+	secondInput.oninput = fromMtoK ;
  
 function fromKtoM(){
-	firstVar = 0.62137119 , secondVar = 1.609344 ;
+	var firstVar = 0.62137119 ;
 	secondInput.value =  (Math.round(parseFloat(firstInput.value) * firstVar * 100) /100) || 0   ;
 };
 function fromMtoK(){
-	firstVar = 0.62137119 , secondVar = 1.609344 ;
+	var secondVar = 1.609344 ;
 	firstInput.value =  (Math.round(parseFloat(secondInput.value) * secondVar * 100) /100) || 0  ;
 };
+function fromKtoP(){
+	var firstVar = 0.45359237 ;
+	secondInput.value =  (Math.round(parseFloat(firstInput.value) * firstVar * 100) /100) || 0   ;
+};
+function fromPtoK(){
+	var secondVar = 2.20462262 ;
+	firstInput.value =  (Math.round(parseFloat(secondInput.value) * secondVar * 100) /100) || 0  ;
+};
+
 
 function change(){
 	if(firstInput.placeholder === 'Enter kilometers here') {
 	firstInput.placeholder = 'Enter kilograms here' ;
 	secondInput.placeholder = 'Enter pounds here' ;
 	weightButton.innerHTML = "kilometers/Miles" ;
-		
+	firstInput.oninput = fromKtoP ;
+	secondInput.oninput = fromPtoK ;
+	
 	} else if(firstInput.placeholder ===  'Enter kilograms here') {
 	firstInput.placeholder = 'Enter kilometers here' ;
 	secondInput.placeholder = 'Enter miles here' ;	
-	weightButton.innerHTML = "kilograms/pounds"
-	}
+	weightButton.innerHTML = "kilograms/pounds" ;
+	firstInput.oninput = fromKtoM ;
+	secondInput.oninput = fromMtoK ;
+	} 
 	
 } ;
 $('#weightButton').on('click' , change)  ;
+$('#clear').on('click' , function(){
+	firstInput.value = '' ;
+	secondInput.value = '' ;
+} )
 
